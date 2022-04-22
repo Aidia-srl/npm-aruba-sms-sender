e35d50e4ab5e09995b6280c228cf39d3a3165b95
Adel Massimo Ramadan <adel.massimo.ramadan@gmail.com>
2022-04-22 15:42:59 +0200

    published

--- main.js
+++ main.js
@@ -2,8 +2,7 @@ var BASEURL = 'https://smspanel.aruba.it/API/v1.0/REST/';
 
 var MESSAGE_HIGH_QUALITY = "N";
 
-import { default as axios } from 'axios';
-
+const axios = require('axios');
 /**
  * Authenticates the user given it's username and password.  Callback
  * is called when completed. If error is false, then an authentication
@@ -30,19 +29,20 @@ async function _login(username, password) {
 /**
  * Sends an SMS message
  */
-async function _sendSMS(auth, recipients, message_text) {
+async function _sendSMS(auth, recipients, message_text, sender) {
     try {
         
         const res = await axios.post(BASEURL + 'sms',{
             "returnCredits": true,
             "recipient": recipients,
             "message": message_text,
+            "sender": sender,
             "message_type": MESSAGE_HIGH_QUALITY
         }, {
             headers: { 'user_key' : auth.user_key, 'Session_key' : auth.session_key },
         });
         
-        return res.status == 200
+        return true
         
     } catch (error) {
         console.log('error sending sms')
@@ -52,8 +52,10 @@ async function _sendSMS(auth, recipients, message_text) {
     }
 }
 
-export async function sendSMS(user, password, recipients, message_text) {
+async function sendSMS(user, password, recipients, message_text, sender) {
     const auth = await _login(user, password);
-    const success = await _sendSMS(auth, recipients, message_text);
+    const success = await _sendSMS(auth, recipients, message_text, sender);
     return {success}
 }
+
+exports.sendSMS = sendSMS;
